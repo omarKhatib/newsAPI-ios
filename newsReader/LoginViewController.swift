7eb739a558b40331be685f45f9f1ba382bcd61b8
omarkhatib <omarkh1995@yahoo.com>
2017-08-21 05:01:25 -0700

    add login storyboard and edit app delegate

---
+++ newsReader/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  newsReader
+//
+//  Created by omar kh on 8/21/17.
+//  Copyright © 2017 omar kh. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+
+class LoginViewController : UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+}
+
+}
